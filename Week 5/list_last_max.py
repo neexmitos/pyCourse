974fe4c96cf006dd2a8178f0d6ea787b73d94a42
NeeX <nexmitos@gmail.com>
2021-11-03 22:05:10 +0300

    week 5

--- Week 5/list_last_max.py
+++ Week 5/list_last_max.py
@@ -1,10 +1,13 @@
 numList = list(map(int, input().split()))
 count = 0
-indNum = 0
+indNum = numList[0]
+ind = 0
+ind1 = 0
 for i in range(0, len(numList)):
     if numList[i] > indNum:
         indNum = numList[i]
-if numList.count(indNum) > 1:
-    print(indNum, numList.index(indNum, numList.index(indNum) + 1))
-else:
-    print(indNum, numList.index(indNum))
+for y in numList:
+    if y == indNum:
+        ind1 = count
+    count += 1
+print(indNum, ind1)
