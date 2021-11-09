ce5c08efe0c0baeec51e805ba34d03dc895e4850
NeeX <nexmitos@gmail.com>
2021-11-09 23:11:07 +0300

    week5

--- Week 5/bolshe_pred.py
+++
@@ -1,6 +0,0 @@
-numList = list(map(int, input().split()))
-count = 0
-for i in numList:
-    if numList[0] <= (numList[len(numList) - 1]) and i > numList[count - 1]:
-        print(i, end=' ')
-    count += 1
