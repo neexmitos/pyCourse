4e6d39f74910b28d1308b9ca2c8f16fcdeba7352
neexmitos <neexmitos@gmail.com>
2021-11-10 11:25:51 +0300

    week5

--- Week 5/revlist_manual.py
+++ Week 5/revlist_manual.py
@@ -1,6 +1,6 @@
 numList = list(map(int, input().split()))
 decr = -1
-for i in range(0, len(numList)):
+for i in range(0, len(numList) - 3):
     numList[i] = numList[decr]
     decr -= 1
 print(numList)
