c2b2f56ff6e2c6e04b348161b66f89c326cc765e
neexmitos <neexmitos@gmail.com>
2021-11-15 16:48:12 +0300

    week 5

--- Week 5/Shereng.py
+++ Week 5/Shereng.py
@@ -1,21 +1,11 @@
 l = list(map(int, input().split()))
 x = int(input())
-l1 = []
-for i in l:
-    if x - i < 0:
-        l1.append((x - i) * -1)
-    else:
-        l1.append(x - i)
-min = l1[0]
-minInd = 0
+insInd = 0
+countIf = 1
 count = 1
-zCount = 0
-for y in l1:
-    if y <= min:
-        min = y
-        minInd = count + 1
+for i in l:
+    if x % i < x and x % i != 0:
+        countIf += 1
     count += 1
-if x <= l[0]:
-    print(minInd)
-else:
-    print(1)
+insInd = (count - countIf) + 1
+print(insInd)
