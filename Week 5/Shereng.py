3dffed2d7fa490acadab2479fc79f4d5cac1ac9d
neexmitos <neexmitos@gmail.com>
2021-11-16 14:30:06 +0300

    week 5

--- Week 5/Shereng.py
+++ Week 5/Shereng.py
@@ -1,11 +1,9 @@
 l = list(map(int, input().split()))
 x = int(input())
-insInd = 0
-countIf = 1
+ind = 1
 count = 1
 for i in l:
-    if x % i < x and x % i != 0:
-        countIf += 1
+    if x <= i:
+        ind += 1
     count += 1
-insInd = (count - countIf) + 1
-print(insInd)
+print(ind)
