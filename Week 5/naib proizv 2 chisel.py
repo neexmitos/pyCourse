e3f01f9dc57ba87df8ac7a2204009b822cbf06d4
neexmitos <neexmitos@gmail.com>
2021-11-22 15:14:55 +0300

    week 5

--- Week 5/naib proizv 2 chisel.py
+++ Week 5/naib proizv 2 chisel.py
@@ -1,29 +1,22 @@
 l = list(map(int, input().split()))
-max2 = l[0]
-indMax2 = 0
-max = l[0]
-a = 0
-count = 0
-for i in range(0, len(l)):
-    if l[i] < 0:
-        a = l[i] * -1
-        l[i] = a
-for i in range(0, len(l)):
-    if l[i] > max:
-        max = l[i]
-        indMax = i
-for y in range(0, len(l)):
-    if l[y] < max and l[y] > max2:
-        max2 = l[y]
-
-print(max, max2)
-# while countWh < len(l):
-#     for i in l:
-#         comp = l[countWh] * i
-#
-#         # ind += 1
-#         print(comp)
-#     countIf = countWh
-#     countWh += 1
-
-# print(*l)
+if len(l) <= 2 and l[0] < l[1]:
+    print(l[0], l[1])
+elif len(l) <= 2 and l[0] > l[1]:
+    print(l[1], l[0])
+else:
+    maxi = max(l)
+    l.remove(maxi)
+    maxi1 = max(l)
+    mini = min(l)
+    l.remove(mini)
+    mini1 = min(l)
+    if maxi * maxi1 > mini * mini1:
+        if maxi < maxi1:
+            print(maxi, maxi1)
+        else:
+            print(maxi1, maxi)
+    else:
+        if mini < mini1:
+            print(mini, mini1)
+        else:
+            print(mini1, mini)
